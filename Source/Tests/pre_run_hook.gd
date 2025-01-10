ae9c4119cf58786acb7768cb117b555656efab8c
Fabricio Manrique <fabemanrique@hotmail.com>
2025-01-10 18:35:48 -0300

    [BTC-4] - Create a Jenkins job to run code coverage on Godot project
    Fixing Godot crash when running tests
    
    Resolves #4

--- Source/Tests/pre_run_hook.gd
+++ Source/Tests/pre_run_hook.gd
@@ -1,7 +1,7 @@
 extends "res://addons/gut/hook_script.gd"
 
 const Coverage = preload("res://addons/coverage/coverage.gd")
-const EXCLUDE_PATHS = ["res://addons/*", "res://tests/*", "res://contrib/*"]
+const EXCLUDE_PATHS = ["res://addons/*", "res://tests/*", "res://contrib/*", "res://Tests/pre_run_hook.gd", "res://Tests/post_run_hook.gd"]
 
 
 func run():
