c7a50618e48d3bad665b2dc49a3280e13c50a04d
Fabricio Manrique <fabemanrique@hotmail.com>
2025-02-27 19:10:51 -0300

    [BTC-109] - Implement some basic layers and masks so projectiles can collide with level
    Added some layers and masks to make the collision be more precise and we destroy the projectile after collision
    Added some primitives to the world environment
    
    TIP: issue areas don't collide with static bodies
    If you also found yourself here and are using Jolt3d. The answer is in the advanced project settings of Jolt3D. You will need to enable "Areas detect static bodies". This is not enabled by default.
    
    Resolves #109

--- Source/Scripts/Projectiles/projectile.gd
+++ Source/Scripts/Projectiles/projectile.gd
@@ -16,4 +16,19 @@ func _physics_process(delta: float) -> void:
 #here we check if the projectile left the screen to remove it
 #this is done using the VisibleOnScreenNotifier3D node
 func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
+	_destroy_projectile()
+
+
+# if we collided with other body
+func _on_body_entered(_body: Node3D) -> void:
+	# TODO: if(body is ControllableEntity):
+		# TODO: var controllable_entity : ControllableEntity = body as ControllableEntity
+		# TODO: here we should take damage: controllable_entity.take_damage()
+	#we destroy the projectile after it collides with anything
+	_destroy_projectile()	
+
+
+# for now we only remove the node from the tree
+# but we can spawn particles, play sound, etc
+func _destroy_projectile() -> void:
 	queue_free()
