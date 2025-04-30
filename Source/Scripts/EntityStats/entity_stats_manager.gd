912252958711eca33657114afaf8a663d75731ac
Fabricio Manrique <fabemanrique@hotmail.com>
2025-04-30 15:00:08 -0300

    [BTC-126] - Improve entity stats modifiers to not use .duplicate
    Refactored Stats:
      - We free up resources for timer
      - Removed not needed code
    
    Resolves #126

--- Source/Scripts/EntityStats/entity_stats_manager.gd
+++ Source/Scripts/EntityStats/entity_stats_manager.gd
@@ -4,22 +4,22 @@ extends Node
 @export_group("Stats")
 ## base entity stats before any modification
 @export var _base_entity_stats : EntityStats
-@export_group("Stats Modifiers")
 
 ## a list of different modifiers for the current stats
 var _modifiers : Array[EntityStatsModifier]
 
 ## we are going to work over this copy to not affect the real resource
-var _current_base_entity_stats : EntityStats:
-	get():
-		# the first time we generate a copy to not affect real stats
-		# and we apply the modifiers
-		if _current_base_entity_stats == null:
-			_current_base_entity_stats = _base_entity_stats.duplicate()
-			_apply_modifiers()
-		return _current_base_entity_stats
+var _current_stacked_entity_stats : EntityStats
 
 
+func _ready() -> void:
+	# we apply the modifiers
+	_apply_modifiers()
+
+
+## we add the modifier and we reapply the others left
+## NOTE: that this implementation probably won't work if the have a modifier that is 
+## increasing or decreasing its modified value because we reapply the modifiers again
 func add_modifier(new_modifier : EntityStatsModifier) -> void:
 	new_modifier.init(self)
 	new_modifier.on_modifier_depleted.connect(_modifier_depleted)
@@ -27,6 +27,9 @@ func add_modifier(new_modifier : EntityStatsModifier) -> void:
 	_apply_modifiers()
 
 
+## we remove the modifier and we reapply the others left
+## NOTE: that this implementation probably won't work if the have a modifier that is 
+## increasing or decreasing its modified value because we reapply the modifiers again
 func remove_modifier(modifier : EntityStatsModifier) -> void:
 	modifier.on_modifier_depleted.disconnect(_modifier_depleted)
 	_modifiers.erase(modifier)
@@ -35,14 +38,17 @@ func remove_modifier(modifier : EntityStatsModifier) -> void:
 
 ## we apply all entity stats modifiers
 func _apply_modifiers() -> void:
+	# we generate a copy to not affect real stats and we apply the modifiers
+	_current_stacked_entity_stats = _base_entity_stats.duplicate()
+	# we apply all the modifiers
 	for modifier in _modifiers:
 		# we "decorate" the current stats with this current modifier
-		_current_base_entity_stats = modifier.apply(_current_base_entity_stats)
+		_current_stacked_entity_stats = modifier.apply(_current_stacked_entity_stats)
 
 
 ## we get the base stats with all entity stats modifiers applied
 func entity_stats() -> EntityStats:
-	return _current_base_entity_stats
+	return _current_stacked_entity_stats
 
 
 ## when we deplete a modifier, we remove it from the list
--- Source/Scripts/EntityStats/timed_entity_stats_modifier.gd
+++ Source/Scripts/EntityStats/timed_entity_stats_modifier.gd
@@ -4,24 +4,31 @@ extends EntityStatsModifier
 ## how much time this stats modifier will be applied
 @export_range(0, 60) var duration : float = 3
 
-#internal timer for deplete stats modifier
+## internal timer for deplete this stats modifier
 var _timer : Timer
 
 
 ## we are going to start a timer for this
 func init(owner: Node) -> void:
-	_timer = setup_timer()
+	# we create an initialize the timer
+	setup_timer()
+	# we add it to the scene tree as child of the stats modifier owner
 	owner.add_child(_timer)
+	# we start the timer
 	_timer.start()
 
-
-func setup_timer() -> Timer:
-	var timer : Timer = Timer.new()
-	timer.one_shot = true
-	timer.wait_time = duration
-	timer.timeout.connect(_on_timer_timeout)
-	return timer
+## here we create and set the signals for the timer
+func setup_timer() -> void:
+	_timer = Timer.new()
+	# modifier should be used only once
+	_timer.one_shot = true
+	_timer.wait_time = duration
+	# we connect the timeout signal to let our nodes know when modifier is depleted
+	_timer.timeout.connect(_on_timer_timeout)
 
 
 func _on_timer_timeout() -> void:
+	# we trigger the notification that this modifier is depleted
 	on_modifier_depleted.emit(self)
+	# we remove the timer
+	_timer.queue_free()
