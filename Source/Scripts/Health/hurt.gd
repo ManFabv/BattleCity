12ecae7b9a075531ecaf4a5e4d10ebf94a13a944
Fabricio Manrique <fabemanrique@hotmail.com>
2025-05-25 17:37:10 -0300

    Removed test print statement

--- Source/Scripts/Health/hurt.gd
+++ Source/Scripts/Health/hurt.gd
@@ -22,7 +22,6 @@ func _ready() -> void:
 
 ## if we collided with other body
 func _on_area_entered(_body: Health) -> void:
-	print("AKSKALJSLKAJS")
 	# we take damage when the
 	if _body != null:
 		_body.take_damage(_damage_stats.damage)
