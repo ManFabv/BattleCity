61495ece29d1f3bebca267a24741f06cd2637d6e
Fabricio Manrique <fabemanrique@hotmail.com>
2026-07-14 20:26:34 -0300

    Refactored health stats to be part of the controllable entity

--- Source/Scripts/Health/health.gd
+++ Source/Scripts/Health/health.gd
@@ -1,28 +1,28 @@
 class_name Health
 extends Area3D
 
-@export var _health_stats: HealthStats
+var health_stats: HealthStats
 
 
-func _ready() -> void:
+func initialize(_on_health_changed : Callable, _on_dead : Callable, 
+		_health_stats: HealthStats) -> void:
+	#we save the health
+	health_stats = _health_stats.duplicate()
 	# we start the entity with the max health
-	_health_stats.initialize()
-
-
-func initialize(_on_health_changed : Callable, _on_dead : Callable) -> void:
+	health_stats.initialize()
 	#we listen to health change events
-	_health_stats.on_health_changed.connect(_on_health_changed)
+	health_stats.on_health_changed.connect(_on_health_changed)
 	#we listen to entity dead event
-	_health_stats.on_dead.connect(_on_dead)
+	health_stats.on_dead.connect(_on_dead)
 
 
 ## here we take damage and emit the corresponding signal if player is dead
 func take_damage(damage_stats : DamageStats) -> void:
 	# we update the current health substracting the damage
-	_health_stats.current_health -= damage_stats.damage
+	health_stats.current_health -= damage_stats.damage
 
 
 ## here we take heal amount
 func take_heal(heal_points : int) -> void:
 	# we update the current health adding the heal
-	_health_stats.current_health += heal_points
+	health_stats.current_health += heal_points
