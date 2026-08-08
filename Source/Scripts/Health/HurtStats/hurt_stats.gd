3be46e4cdd8649f7f7a41b44774ff39d87f3843a
Fabricio <fabemanrique@hotmail.com>
2026-08-08 10:27:45 -0300

    We moved logic from Health and hurt resources to the heaalth and hurt components

--- Source/Scripts/Health/HurtStats/hurt_stats.gd
+++ Source/Scripts/Health/HurtStats/hurt_stats.gd
@@ -1,11 +1,6 @@
 class_name DamageStats
 extends Resource
 
-## We use a local copy because we don't want the resource to be shared 
-## between different entities, we want each entity to have its own values
-func _init() -> void:
-	resource_local_to_scene = true
-
 
 ## damage point to make to target
 @export_range(1, 100) var damage : int = 10:
