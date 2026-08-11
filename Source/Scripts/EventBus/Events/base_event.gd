b9a7f804680566af9e993ad34e52824051b46cde
Fabricio Manrique <fabemanrique@hotmail.com>
2026-08-11 16:52:14 -0300

    [BTC-201] - Fix the bug where the shots are deleted when the enemy is killed
    - We check that the base event signal is connected before subscribe/unsubscribe
    - ControllableEntity now unsubscribe from the base events when the entity is destroyed

--- Source/Scripts/EventBus/Events/base_event.gd
+++ Source/Scripts/EventBus/Events/base_event.gd
@@ -12,10 +12,19 @@ func emit() -> void:
 
 
 # we connect the method from the signal
-func subscribe(method: Callable) -> void:
-	_event_signal.connect(method)
+func subscribe(method: Callable, on_unsubscribe_requested: Signal) -> void:
+	if not _is_event_connected(method):
+		_event_signal.connect(method)
+		# we grab the current method using bind so the unsubscribe knows
+		# which method should disconnect
+		on_unsubscribe_requested.connect(unsubscribe.bind(method))
 
 
 # we disconnect the method from the signal
 func unsubscribe(method: Callable) -> void:
-	_event_signal.disconnect(method)
+	if _is_event_connected(method):
+		_event_signal.disconnect(method)
+
+
+func _is_event_connected(method: Callable) -> bool:
+	return _event_signal.is_connected(method)
