bce6c9b6e08033745d7ba6263fe452128b044f87
Fabricio Manrique <fabemanrique@hotmail.com>
2026-08-11 17:42:19 -0300

    [BTC-201] - Fix the bug where the shots are deleted when the enemy is killed
    - Renamed so the container script can hold more objects in the future

--- Source/Scripts/Container/node_container.gd
+++ Source/Scripts/Container/node_container.gd
@@ -1,14 +1,14 @@
 extends Node3D
-class_name ProjectileContainer
+class_name NodeContainer
 
 ## we are going to listen this event so we can parent the projectiles
 ## to this object avoiding to remove projectiles when their owners are removed
-@export var _on_projectile_spawned: BaseEvent
+@export var _on_node_spawned: BaseEvent
 
 
 func _ready() -> void:
 	# We start listening to the event
-	_on_projectile_spawned.subscribe(_parent_projectile, tree_exited)
+	_on_node_spawned.subscribe(_parent_projectile, tree_exited)
 
 
 func _parent_projectile(projectile: Projectile) -> void:
