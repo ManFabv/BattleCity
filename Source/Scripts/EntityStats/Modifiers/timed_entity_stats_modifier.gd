c9051618b94d411c4f0d4f8c0634af55404055a7
Fabricio Manrique <fabemanrique@hotmail.com>
2026-08-21 19:34:33 -0300

    [BTC-211] - Implement a Timer Manager for Reusable Timed Events
    - Refactored timed entity stats modifier to make use of the new timers

--- Source/Scripts/EntityStats/Modifiers/timed_entity_stats_modifier.gd
+++ Source/Scripts/EntityStats/Modifiers/timed_entity_stats_modifier.gd
@@ -1,22 +1,21 @@
 class_name TimedEntityStatsModifier
 extends EntityStatsModifier
 
+## Base Event we will need to handle the request for the timers
+@export var timer_requested : BaseEvent
+
 ## how much time this stats modifier will be applied
 @export_range(0, 60) var _duration : float = 3
 
 
-## Starts the lifetime of this modifier using the SceneTreeTimer
-func initialize(_owner_node: Node) -> void:
-	setup_timer(_owner_node)
-	
-func setup_timer(_owner_node: Node) -> void:
-	# Create a SceneTreeTimer directly from the tree. 
-	# It automatically starts counting down and destroys itself on timeout.
-	var _scene_timer : SceneTreeTimer = _owner_node.get_tree().create_timer(_duration)
-	# Connect the timeout signal directly to our depletion logic
-	_scene_timer.timeout.connect(_on_timer_timeout)
+## Starts the lifetime of this modifier using the Timer Manager
+func initialize(owner_node: Node) -> void:
+	var timer_context : TimerContext = TimerContext.new(_duration, false, _on_timer_timeout, owner_node.tree_exited)
+	timer_requested.emit(timer_context)
 
 
+## this method is called when the timer reaches its timeout
+## so we say that the modifier is depleted
 func _on_timer_timeout() -> void:
 	# Trigger the notification that this modifier is depleted
 	on_modifier_depleted.emit(self)
