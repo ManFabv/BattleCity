44e062f724c2191dca3db36506e723156b2ff2f7
Fabricio Manrique <fabemanrique@hotmail.com>
2026-09-08 17:47:02 -0300

    [BTC-64] - Create Enemy Spawner
    - Renamed node_container to projectile container scene
    - Created on enemy spawned base event
    - Added enemy spawner and enemy container into world scene
    - Spawner notifies the container to add the enemies to the tree
    - We use a timer and a random float value to spawn enemies
    
    Resolves #64

---
+++ Spawner/Scripts/enemy_spawner.gd
@@ -0,0 +1,32 @@
+extends Node3D
+
+## list of enemies availables for spawn
+@export var _enemies: Array[PackedScene]
+
+## the event where we notify that an enemy should be added to the tree
+@export var _on_enemy_spawned : BaseEvent
+
+## the event to request a timer
+@export var _on_timer_requested : BaseEvent
+
+
+## TODO: this logic will be improved: we setup the initial spawn logic. 
+func _ready() -> void:
+	# we take a random time to spawn a new enemy
+	var time_between_spawns : float = randf_range(5, 10)
+	# we create a timer to handle when we want to spawn a new enemy
+	var timer_context : TimerContext = TimerContext.create_loop(time_between_spawns, _spawn_enemy, tree_exited)
+	# we subscribe the timer so it can start ticking
+	_on_timer_requested.emit(timer_context)
+
+
+## TODO: this logic will be improved: we instantiate a new enemy in the level
+func _spawn_enemy() -> void:
+	# we select a random enemy
+	var enemy_index : int = randi_range(0, _enemies.size() - 1)
+	# we instantiate it
+	var enemy : ControllableEntity = _enemies[enemy_index].instantiate() as ControllableEntity
+	# we notify that a new enemy is spawned so the container can add it to the tree
+	_on_enemy_spawned.emit(enemy)
+	# we move the enemy to the spawner position
+	enemy.global_position = global_position
