ee60e04c3c56125afba45c876bc148ff1bf8684c
Fabricio Manrique <fabemanrique@hotmail.com>
2026-09-19 11:28:43 -0300

    Refactored instances

--- EntityStats/Scripts/Modifiers/Instances/speed_entity_stat_modifier_instance.gd
+++ EntityStats/Scripts/Modifiers/Instances/speed_entity_stat_modifier_instance.gd
@@ -1,2 +1,5 @@
 class_name SpeedEntityStatModifierInstance
 extends TimedEntityStatsModifierInstance
+
+## this class represents a speed-specific entity stats modifier that is timed
+## No specific logic required
